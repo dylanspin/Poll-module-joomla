9ce05ec04d33424caa6e796cbe7ebe8439697399
dylan <dylanspin100@hotmail.com>
2020-01-21 14:00:38 +0100

    Git fix
    Push ging fout

--- mod_poll/tmpl/default.php
+++ mod_poll/tmpl/default.php
@@ -9,15 +9,11 @@
     $achtergrond = $params->get('module_kleur');
     $textKleur = $params->get('text_kleur');
 
-    $helper = new ModPoll();//helper class 
+    $helper = new ModPoll();//helper class
 
-<<<<<<< HEAD
     if(!$helper->select($idVraag)){//maakt een nieuwe row aan in de database mocht de aangeven nummer nog niet bestaan
-=======
-    if(!$helper->select($idVraag)){//maakt een nieuwe aan mocht de aangeven nummer nog niet bestaan
->>>>>>> master
         $helper->insert($vraag,$idVraag);
-    } 
+    }
 
     $data = $helper->select($idVraag);
     $doc = JFactory::getDocument();
@@ -28,11 +24,7 @@
     $css = ".PollVraag{
               background-color:".$achtergrond.";
               color: ".$textKleur.";
-<<<<<<< HEAD
             }";//add extra css
-=======
-            }";
->>>>>>> master
 
     $doc->addStyleDeclaration($css);//add de css aan style.css
 
@@ -48,23 +40,22 @@
             $stats = [" ",$data->antwoord1,$data->antwoord2,$data->antwoord3,$data->antwoord4];//pakt de score van alle 4 antwoorden
             $stats[$_POST['pollAntwoord']] += 1;//doet een bij de oude aantal score
             $helper->update($idVraag,$stats[$_POST['pollAntwoord']],"antwoord".$_POST['pollAntwoord']);//updates de values
-            $_SESSION['voted'] = true;//set session 
-<<<<<<< HEAD
+            $_SESSION['voted'] = true;//set session
             $helper->refresh();//refresht
         }
     }
 ?>
 
 
-  <form class="poll" method="post"><!--Breede poll met open functie -->
+  <form class="poll" method="post">
     <div class="PollVraag">
       <div class="VraagInner">
         <?php echo $vraag; ?>
       </div>
-    </div>    
+    </div>
     <div class="closePoll" onclick="openPoll()" id='icon'><i class="fa fa-minus" aria-hidden="true"></i></div>
     <div class="innerPoll" id='innerPoll'>
-      <?php 
+      <?php
           if(isset($_SESSION['voted'])){
               for($i=1; $i<=4; $i++){//mischien een message van bedankt voor het stemmen
                   echo "<div class='procent'><div class='overlay' style='width:$procenten[$i]%;'></div>".$procenten[$i]."%</div>";
@@ -73,29 +64,8 @@
           else{
               for($i=1; $i<=4; $i++){
                   echo "<button name='pollAntwoord' class='pollAntwoorden' value='$i'>$antwoorden[$i]</button>";
-              } 
+              }
           }
-=======
-            $helper->refresh();//refres
-        }
-    }
-?>
-  <form class="poll" method="post"><!--Breede poll met open functie -->
-    <div class="PollVraag"><?php echo $vraag; ?></div>
-    <div class="closePoll" onclick="openPoll()" id='icon'><i class="fa fa-minus" aria-hidden="true"></i></div>
-    <div class="innerPoll" id='innerPoll'>
-      <?php 
-        if(isset($_SESSION['voted'])){
-            for($i=1; $i<=4; $i++){//mischien een message van bedankt voor het stemmen
-                echo "<div class='procent'><div class='overlay' style='width:$procenten[$i]%;'></div>".$procenten[$i]."%</div>";
-            }
-        }
-        else{
-            for($i=1; $i<=4; $i++){
-                echo "<button name='pollAntwoord' class='pollAntwoorden' value='$i'>$antwoorden[$i]</button>";
-            } 
-        }
->>>>>>> master
        ?>
      </div>
      <input type="text" value="" name='valuePoll' class="pollFake" id='pollfake'>
